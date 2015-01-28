7df8c7d9145912c525c6ca221c4872a31e84964f
Nick Spelt <nickstr@nickstr.nl>
2015-01-28 16:23:27 +0100

    HTML raffler

---
+++ nickspelt-html/gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    rename = require('gulp-rename'),
+    preprocess = require('gulp-preprocess'),
+    minimist = require('minimist');
+
+
+var acceptedOptions = {
+  string: 'winner',
+  default: { winner: 'test subject' }
+};
+
+var options = minimist(process.argv.slice(2), acceptedOptions);
+
+gulp.task('css', function () {
+    return gulp.src('scss/style.scss')
+    .pipe(sass({errLogToConsole: true}))
+    .pipe(preprocess({context: {WINNER: options.winner}}))
+    .pipe(autoprefixer('last 4 version'))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('default', ['css'], function () {
+    gulp.watch("scss/*.scss", ['css']);
+});
+

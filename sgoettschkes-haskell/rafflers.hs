1ffa497a5553c402f97042bfacc00ea963197418
Sebastian Göttschkes <sebastian.goettschkes@googlemail.com>
2015-01-28 14:55:21 +0000

    Removing the arg value as well with some magic `<==`

--- sgoettschkes-haskell/rafflers.hs
+++ sgoettschkes-haskell/rafflers.hs
@@ -1,11 +1,9 @@
-import Control.Monad
 import System.Environment
 import System.IO
 import System.Random 
 
 main = do 
-    arg : _ <- getArgs
-    contents <- openFile arg ReadMode >>= hGetContents
+    contents <- hGetContents =<< flip openFile ReadMode . head =<< getArgs
     
     gen <- getStdGen
     print $ lines contents !! (fst $ randomR (0,length $ lines contents) gen)

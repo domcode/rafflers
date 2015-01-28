42c077246de04027ec606c84238238802e01bbd9
lucasvanlierop <lucas@vanlierop.org>
2015-01-28 22:52:08 +0100

    Figured out how to access a command line argument

--- lucasvanlierop-cobol/raffler.cbl
+++ lucasvanlierop-cobol/raffler.cbl
@@ -1,6 +1,25 @@
-        IDENTIFICATION DIVISION.
-        PROGRAM-ID. DOMCODE-RAFFLER.
-        AUTHOR. Lucas van Lierop.
-        PROCEDURE DIVISION.
-            DISPLAY 'Ready to raffle!'.
-        STOP RUN.
+        >>source format is free
+IDENTIFICATION DIVISION.
+    PROGRAM-ID. DOMCODE-RAFFLER.
+    AUTHOR. Lucas van Lierop.
+
+data division.
+    working-storage section.
+        01 file-name PIC x(50).
+PROCEDURE DIVISION.
+    DISPLAY 'Ready to raffle!'.
+    PERFORM 100-INITIALIZE.
+STOP RUN.
+
+100-INITIALIZE.
+    ACCEPT file-name FROM ARGUMENT-VALUE
+        ON EXCEPTION
+            DISPLAY
+                "Attempt to read beyond end of command line"
+                UPON SYSERR
+            END-DISPLAY
+        NOT ON EXCEPTION
+            DISPLAY
+                "reading " file-name
+            END-display
+    END-ACCEPT.

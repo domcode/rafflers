5d0a121a5b366ea4dc3683ac38f3b3db86cca12c
Shawn McCool <shawn@heybigname.com>
2015-01-28 16:38:56 +0100

    remove object context

--- shawnmccool-scala/Raffler.scala
+++ shawnmccool-scala/Raffler.scala
@@ -1,29 +1,22 @@
+import scala.util.Random
+import scala.io.Source
 
-object Raffler {
-  def main(args: Array[String]) {
-    import scala.util.Random
-    import scala.io.Source
-
-    def raffleUntil(contestants: List[String], done: () => Boolean) = {
-      def chooseWinner(contestants: List[String]): Unit = {
-        if ( ! contestants.isEmpty) {
-          println(contestants.head)
-          if ( ! done()) chooseWinner(contestants.tail)
-        }
-      }
-      chooseWinner(contestants)
+def raffleUntil(contestants: List[String], done: () => Boolean) = {
+  def chooseWinner(contestants: List[String]): Unit = {
+    if ( ! contestants.isEmpty) {
+      println(contestants.head)
+      if ( ! done()) chooseWinner(contestants.tail)
     }
-
-    println("Press enter to select a winner, q on a blank line to quit.");
-    Console.in.read
-
-    try {
-      val contestants = Random.shuffle(Source.fromFile(args(0)).getLines()).toList
-      raffleUntil(contestants, () => Console.in.read == 'q')
-    } catch {
-      case _: Throwable => println("Could not load the file.")
-    }
-
   }
+  chooseWinner(contestants)
 }
 
+println("Press enter to select a winner, q on a blank line to quit.");
+Console.in.read
+
+try {
+  val contestants = Random.shuffle(Source.fromFile(args(0)).getLines()).toList
+  raffleUntil(contestants, () => Console.in.read == 'q')
+} catch {
+  case _: Throwable => println("Could not load the file.")
+}

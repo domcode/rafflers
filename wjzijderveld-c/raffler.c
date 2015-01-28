014f844fb03c60d94199b97219355a74b55695ab
Willem-Jan <wjzijderveld@gmail.com>
2015-01-28 21:25:34 +0100

    Raffler in C

--- wjzijderveld-c/raffler.c
+++ wjzijderveld-c/raffler.c
@@ -1,64 +1,50 @@
+#include <sys/time.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-int lineLength(FILE *file) {
-    char c;
-    int count = 0;
-    fpos_t pos;
-
-    fgetpos(file, &pos);
-
-    while (!feof(file)) {
-        c = fgetc(file);
-        if (c == '\n') {
-            fsetpos(file, &pos);
-            return count;
-        }
-        count++;
-    }
-}
 
 int main(int argc, char *argv[]) {
 
     if (argc < 2) {
         printf("Didn't you forget something?\n");
-        return 1;
+        exit(EXIT_FAILURE);
     }
 
     FILE *file = fopen(argv[1], "r");
     if (0 == file) {
         printf("Your input file is broken!\n");
-        return 2;
+        exit(EXIT_FAILURE);
     }
 
     int nameCounter = 0; 
-    char c;
-    while (!feof(file)) {
-       c = fgetc(file);
-       if (c == '\n') {
-           nameCounter++;
-       }
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    while ((read = getline(&line, &len, file)) != -1) {
+       nameCounter++;
     }
 
     printf("I counted %d names to choose from!\n", nameCounter);
 
+    struct timeval t1;
+    gettimeofday(&t1, NULL);
+    srand(t1.tv_usec * t1.tv_sec);
+    int luckyWinner = rand() % nameCounter;
+
+    printf("Lucky number %d\n", luckyWinner);
+
     rewind(file);
 
-    int counter;
-    int nameLength;
-    for (counter = 0; counter < nameCounter; counter++) {
-
-        nameLength = lineLength(file);
-        char name[nameLength];
-
-        int i;
-        for (i = 0; i < nameLength; i++) {
-            name[i] = fgetc(file);
-        }
-        c = fgetc(file); // newline
-
-        printf("Found name %s\n", name);
+    int i;
+    for (i = 0; i < luckyWinner; i++) {
+        read = getline(&line, &len, file);
     }
 
+    printf("Lucky winner is.... %s", line);
+
     fclose(file);
+    if (line) {
+        free(line);
+    }
+
+    exit(EXIT_SUCCESS);
 }

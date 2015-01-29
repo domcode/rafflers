d3754fa5f5db2ecbd27a61badb022771d145bd89
lucasvanlierop <lucas@vanlierop.org>
2015-01-29 13:56:36 +0100

    First version of reading a file

--- lucasvanlierop-cobol/raffler.cbl
+++ lucasvanlierop-cobol/raffler.cbl
@@ -9,17 +9,38 @@ identification division.
       *> ./raffler {filename}
       *> =============================================
 
+environment division.
+    input-output section.
+       file-control.
+           select names-file
+               assign to names-file-name
+               file status is names-file-status
+               organization is line sequential.
 
 data division.
+    file section.
+        fd names-file.
+        01 names-record pic x(80).
+
     working-storage section.
-        01 file-name pic x(50).
+        01 names-file-name pic x(50).
+        01 names-file-status pic x(2).
+        01 names-count pic 9(9) value zero.
+
+       01  name-columns.
+           03  name pic x(32).
+
+       01  display-count pic z,zzz,zzz.
+
 procedure division.
     display 'ready to raffle!'.
     perform 100-initialize.
+       perform 110-read-input-file.
+
 stop run.
 
 100-initialize.
-    accept file-name from argument-value
+    accept names-file-name from argument-value
         on exception
             display
                 "attempt to read beyond end of command line"
@@ -27,7 +48,26 @@ stop run.
             end-display
         not on exception
             display
-                "reading " file-name
+                "reading " names-file-name
             end-display
     end-accept.
 
+110-read-input-file.
+    open input names-file
+
+           read names-file
+
+           perform until names-file-status = '10'
+               add 1 to names-count
+
+               unstring names-record delimited by ',' into
+                   name
+
+               read names-file
+           end-perform
+
+           close names-file
+
+           move names-count to display-count
+           display display-count space 'names'
+           .

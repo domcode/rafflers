1db5bc0e058fe56fbc78e57ef07444fd713f40ca
jmoos <jordymoos@gmail.com>
2015-01-29 08:36:46 +0100

    Little fixes for the lua version

--- rdohms-lua/raffler.lua
+++ rdohms-lua/raffler.lua
@@ -1,17 +1,11 @@
 -- raffler.lua
 
-function tablelength(T)
-  local count = 0
-  for _ in pairs(T) do count = count + 1 end
-  return count
-end
-
 function readFromFile(file)
     local file = io.open(file, "r");
     local arr = {}
 
     for line in file:lines() do
-       table.insert (arr, line);
+        table.insert (arr, line);
     end
 
     io.close(file)
@@ -22,6 +16,6 @@ end
 math.randomseed (os.time())
 
 local participants = readFromFile("participants.txt")
-local winningEntry = math.random(0, tablelength(participants))
+local winningEntry = math.random(1, #participants)
 
-print("The Winner is" .. participants[winningEntry])
+print("The Winner is " .. participants[winningEntry])

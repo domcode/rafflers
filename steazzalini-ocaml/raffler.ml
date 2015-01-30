c89d60e82dbc2496d30eb6b421a28e1d5d836645
Stefano Azzalini <stefano.azzalini@yahoo.com>
2015-01-30 15:38:46 +0100

    use 2 spaces indentation instead of tabs

--- steazzalini-ocaml/raffler.ml
+++ steazzalini-ocaml/raffler.ml
@@ -2,8 +2,8 @@
 let argc = Array.length Sys.argv;;
 
 if argc < 2 then begin
-	print_endline "Did you forgot something?!";
-	exit 1;
+  print_endline "Did you forgot something?!";
+  exit 1;
 end
 
 let filename = Sys.argv.(1);;
@@ -11,21 +11,21 @@ let filename = Sys.argv.(1);;
 let file_exists = Sys.file_exists filename;;
 
 if file_exists = false then begin
-	print_endline "Dazed and Confused...";
-	exit 1;	
+  print_endline "Dazed and Confused...";
+  exit 1;  
 end
 
 (* read the file `f' and store each line into a list *)
 let read_file f = 
-	let lines = ref [] in
-		let chan = open_in f in
-		try
-			while true; do
-				lines := input_line chan :: !lines
-			done; []
-		with End_of_file ->
-			close_in chan;
-			List.rev !lines;;
+  let lines = ref [] in
+    let chan = open_in f in
+    try
+      while true; do
+        lines := input_line chan :: !lines
+      done; []
+    with End_of_file ->
+      close_in chan;
+      List.rev !lines;;
 
 let names = read_file filename;;
 
@@ -34,7 +34,7 @@ let names = read_file filename;;
 let _ = Random.self_init();;
 
 let random_element l =
-    List.nth l (Random.int (List.length l));;
+  List.nth l (Random.int (List.length l));;
 
 let random_name = random_element names;;
 

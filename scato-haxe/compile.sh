2dee70ba5361c36a004c047cbee233220892fc54
Scato Eggen <seggen@ibuildings.nl>
2015-02-01 00:40:29 +0100

    Raffler in HaXe

---
+++ scato-haxe/compile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export PATH=$PATH:/home/vagrant/haxe-3.1.3
+export HAXE_STD_PATH=/home/vagrant/haxe-3.1.3/std
+
+cd /vagrant
+/home/vagrant/haxe-3.1.3/haxe -main Raffler -php raffler-php
+

f1967cd485ee686683d31b98b8dbe17c6ccd9f03
Sebastian Göttschkes <sebastian.goettschkes@googlemail.com>
2015-09-30 21:17:24 +0200

    Fixing index to large bug

--- sgoettschkes-haskell/rafflers.hs
+++ sgoettschkes-haskell/rafflers.hs
@@ -6,4 +6,4 @@ main = do
     contents <- hGetContents =<< flip openFile ReadMode . head =<< getArgs
     
     gen <- getStdGen
-    let lc = lines contents in print $ lc !! (fst $ randomR (0,length lc) gen)
+    let lc = lines contents in print $ lc !! (fst $ randomR (0,(length lc - 1)) gen)

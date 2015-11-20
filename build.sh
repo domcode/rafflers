57a84a64b25bc19a570d333db985003edba5fdc1
Lucas van Lierop <lucas@vanlierop.org>
2015-11-20 14:49:55 +0100

    Dockerized Elixer raffler

--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ set -e
 #docker build -t frankdejonge_rust_raffler frankdejonge-rust
 docker build -t basbl_zsh_raffler basbl-zsh
 docker build -t lucasvanlierop_cobol_raffler lucasvanlierop-cobol
+docker build -t rjkip_elixir_raffler rjkip-elixir
 docker build -t sgoettschkes_haskell_raffler sgoettschkes-haskell

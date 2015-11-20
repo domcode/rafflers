b9bf4c3cec5eab31301f247fa64709c11b607cc9
Lucas van Lierop <lucas@vanlierop.org>
2015-11-20 14:37:28 +0100

    Dockerized Zsh raffler

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 
 # Note that the rust raffle does not compile yet
 #docker build -t frankdejonge_rust_raffler frankdejonge-rust
+docker build -t basbl_zsh_raffler basbl-zsh
 docker build -t lucasvanlierop_cobol_raffler lucasvanlierop-cobol
 docker build -t sgoettschkes_haskell_raffler sgoettschkes-haskell

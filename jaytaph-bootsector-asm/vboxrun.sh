955013af5b9d89fd3f663f7018e3a5b4d27d203f
Lucas van Lierop <lucas@vanlierop.org>
2016-02-02 20:10:35 +0100

    Attempt to run asm raffler in docker

--- jaytaph-bootsector-asm/vboxrun.sh
+++ jaytaph-bootsector-asm/vboxrun.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
+# Creates an empty 1.44MB floppy image
+dd if=/dev/zero of=mr_floppy.img bs=512 count=2880
+
+# Format it
+mkfs.msdos mr_floppy.img
+
+mcopy -i mr_floppy.img /var/names/current ::/NAMES.DAT
+mdir -i mr_floppy.img
+nasm -o bootsector.img raffler.S
+
+# copy bootsector to the start of the floppy image
+dd if=bootsector.img of=mr_floppy.img bs=512 count=1 conv=notrunc
+
+#bochs -f bochsrc -q
+
+#/sbin/vboxconfig
+
 VBM=`which VBoxManage`
 
 TS=`date +"%s"`

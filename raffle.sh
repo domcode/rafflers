f6a76722e6f458f140fbc805d57d6f82cfbd74cc
Remon van de Kamp <rpkamp@gmail.com>
2016-02-03 21:58:40 +0100

    Automatically determine which rafflers are available through 'docker images'

--- raffle.sh
+++ raffle.sh
@@ -16,11 +16,11 @@ NAMES_FILE=`realpath $WORKING_DIR/$1`
 cp $NAMES_FILE $NAMES_DIR/current
 
 # Raffle a raffler ;-)
-declare -a RAFFLER_NAMES=('basbl_zsh' 'lucasvanlierop_cobol' 'rjkip_elixir' 'sgoettschkes_haskell' 'shawnmccool_scala' 'aochagavia_rust')
+declare RAFFLER_NAMES=(`docker images | awk '{print $1}' | grep '_raffler$'`)
 RANDOM_RAFFLER=${RAFFLER_NAMES[$RANDOM % ${#RAFFLER_NAMES[@]} ]}
 
 # run raffler in container with names dir mounted
-CONTAINER_NAME="${RANDOM_RAFFLER}_raffler"
+CONTAINER_NAME="${RANDOM_RAFFLER}"
 echo -e "Raffling using \033[91m$RANDOM_RAFFLER\e[0m"
 echo -e "\033[92mAnd the winner is: "
 docker run -v $NAMES_DIR:/var/names $CONTAINER_NAME

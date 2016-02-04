3a7dbcd6f7adbf69d8dd48ba235826532b1ede0e
Remon van de Kamp <rpkamp@gmail.com>
2016-02-04 13:18:09 +0100

    Checked scripts with shellcheck and implemented suggested changes

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ dockerfiles=$(ls */Dockerfile)
 for file in $dockerfiles; do
 	dir=${file:0:-11}
 	container=${dir/-/_}"_raffler"
-	if [ $(docker images | awk '{print $1}' | grep '^'$container'$' | wc -l) -eq 0 ]; then
+	if [ $(docker images | awk '{print $1}' | grep -c '^'"$container"'$') -eq 0 ]; then
 		echo "Building $container from $dir"
-		docker build -t $container $dir
+		docker build -t "$container" "$dir"
 	else
 		echo "Skipping $container - already exists"
 	fi
--- raffle.sh
+++ raffle.sh
@@ -10,18 +10,18 @@ NAMES_DIR="/tmp/raffler_names"
 mkdir -p $NAMES_DIR
 
 # Copy names file passed as argument to names dir
-SCRIPT_PATH=`realpath $0`
-WORKING_DIR=`dirname ${SCRIPT_PATH}`
-NAMES_FILE=`realpath $WORKING_DIR/$1`
-cp $NAMES_FILE $NAMES_DIR/current
+SCRIPT_PATH=$(realpath "$0")
+WORKING_DIR=$(dirname "$SCRIPT_PATH")
+NAMES_FILE=$(realpath "$WORKING_DIR/$1")
+cp "$NAMES_FILE" "$NAMES_DIR/current"
 
 # Raffle a raffler ;-)
-declare RAFFLER_NAMES=(`docker images | awk '{print $1}' | grep '_raffler$'`)
+declare RAFFLER_NAMES=($(docker images | awk '{print $1}' | grep '_raffler$'))
 RANDOM_RAFFLER=${RAFFLER_NAMES[$RANDOM % ${#RAFFLER_NAMES[@]} ]}
 
 # run raffler in container with names dir mounted
 CONTAINER_NAME="${RANDOM_RAFFLER}"
 echo -e "Raffling using \033[91m$RANDOM_RAFFLER\e[0m"
 echo -e "\033[92mAnd the winner is: "
-docker run -v $NAMES_DIR:/var/names $CONTAINER_NAME
+docker run -v $NAMES_DIR:/var/names "$CONTAINER_NAME"
 echo -e "\e[0m"

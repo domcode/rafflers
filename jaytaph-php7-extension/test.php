efdfe777599bc575c17937fe19565e58722ba7ee
Robin Speekenbrink <robin@kingsquare.nl>
2016-07-05 22:38:12 +0200

    tried to get the extension to work..
    I've updated the raffle script (test.php) in order to dynamically read the given file, add the names.
    
    The problem is... all it currently does is return "yes"
    ?

--- jaytaph-php7-extension/test.php
+++ jaytaph-php7-extension/test.php
@@ -1,8 +1,12 @@
 <?php
 
-$d = new DomCode();
-$d->addname('foo');
-$d->addname('bar');
-$d->addname('baz');
+$filename = !empty($argv[1]) && is_readable($argv[1]) ? realpath($argv[1]) : '';
+if (empty($filename)) {
+    die('please use `php test.php path_to_file`');
+}
 
-echo $d->raffle();
+$d = new DomCode();
+foreach (array_filter(file($filename)) as $name) {
+    $d->addname($name);
+}
+echo $d->raffle().PHP_EOL;

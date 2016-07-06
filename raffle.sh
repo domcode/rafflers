e5a87b5fa0b29c933b6fdb289350931fb544b1cf
Lucas van Lierop <lucas@vanlierop.org>
2016-07-06 12:14:39 +0200

    Let Travis push tags to Docker Hub

--- raffle.sh
+++ raffle.sh
@@ -16,12 +16,12 @@ NAMES_FILE=$(realpath "$WORKING_DIR/$1")
 cp "$NAMES_FILE" "$NAMES_DIR/current"
 
 # Raffle a raffler ;-)
-declare RAFFLER_NAMES=($(docker images | awk '{print $1}' | grep '_raffler$'))
-RANDOM_RAFFLER=${RAFFLER_NAMES[$RANDOM % ${#RAFFLER_NAMES[@]} ]}
+declare RAFFLER_TAGS=($(docker images | awk '{print $1":"$2}' | grep domcode/raffler))
+
+RANDOM_RAFFLER_TAG=${RAFFLER_TAGS[$RANDOM % ${#RAFFLER_TAGS[@]} ]}
 
 # run raffler in container with names dir mounted
-CONTAINER_NAME="${RANDOM_RAFFLER}"
-echo -e "Raffling using \033[91m$RANDOM_RAFFLER\e[0m"
+echo -e "Raffling using \033[91m$RANDOM_RAFFLER_TAG\e[0m"
 echo -e "\033[92mAnd the winner is: "
-docker run -v $NAMES_DIR:/var/names "$CONTAINER_NAME"
+docker run -v $NAMES_DIR:/var/names "$RANDOM_RAFFLER_TAG"
 echo -e "\e[0m"

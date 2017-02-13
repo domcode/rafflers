8f3f5ee3d1626f2f261285f5b049f910c4300597
Alex Rothuis <arothuis@ibuildings.nl>
2017-02-14 00:11:59 +0100

    Add error/usage message for missing file path

--- arothuis-dlang/raffler.d
+++ arothuis-dlang/raffler.d
@@ -6,6 +6,12 @@ import core.stdc.stdlib;
 
 void main(string[] args)
 {
+    if (args.length < 2 || args[1] == "help") {
+        writeln("[ERROR] No file path found.");
+        writeln("   Usage: raffler <FILE_PATH>");
+        exit(1);
+    }
+
     try {
         args[1].readText.strip.splitLines.choice.writeln;
     } catch (FileException e) {
